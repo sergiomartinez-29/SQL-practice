ee02e5276db2836e52d0f9f581965a0d597be394
Sergio René Martínez González <117244891+sergiomartinez-29@users.noreply.github.com>
2025-08-16 19:40:01 -0600

    first commit

---
+++ crear tabla.sql
@@ -0,0 +1,11 @@
+create table users
+(
+	userId int primary key IDENTITY(1,1),
+	name_user varchar(50) not null,
+	surname varchar(50) not null,
+	age int null,
+	init_date date null,
+	email varchar(100) null
+)
+
+select * from users
