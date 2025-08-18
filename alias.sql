f6c2b71731880b806fc181560d7f2a471e253305
Sergio René Martínez González <117244891+sergiomartinez-29@users.noreply.github.com>
2025-08-17 21:16:58 -0600

    17 de agosto
    me gustan las abejas

---
+++ alias.sql
@@ -0,0 +1,3 @@
+select concat(name_user, ' ', surname) as [nombre completo], init_date as 'año de programación' from users
+
+--nombre de tablas super largo y con espacios
---
+++ group by.sql
@@ -0,0 +1,7 @@
+select count(age) as conteo, age from users where age<24 group by age order by age desc
+select max(age)from users group by age
+--agrupar filas con valores iguales.
+
+select * from users
+
+-- se muestran agrupaciones de edades máximas
