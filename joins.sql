e02010e25a3fcc99ee06409db4e8c806b5213a29
Sergio René Martínez González <117244891+sergiomartinez-29@users.noreply.github.com>
2025-09-21 18:04:45 -0600

    Update joins.sql

--- joins.sql
+++ joins.sql
@@ -4,6 +4,6 @@ select * from dni
 select * from users
 left join dni on dni.userId = users.userId
 
-select * from users
+select name_user, names as 'languages he knows' from users
 inner join users_languages on users.userId = users_languages.users_id
 inner join languages on users_languages.languages_id = languages.languages_id
